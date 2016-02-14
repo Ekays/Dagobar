3c80b5f67d1034b1f71ee6d87840c08a1f1cc624
BadCoder <alexandreviper@hotmail.fr>
2016-02-14 02:01:25 +0100

    New C# Project + Splashscreen

---
+++ Dagobar/Forms/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Dagobar
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            timerClose.Start();
+        }
+
+        private void timerClose_Tick(object sender, EventArgs e)
+        {
+            timerClose.Stop();
+
+            //Show next form
+            this.Close();
+        }
+    }
+}
---
+++ Dagobar/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dagobar")]
+[assembly: AssemblyDescription("A Twitch Bot")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BadCoder")]
+[assembly: AssemblyProduct("Dagobar")]
+[assembly: AssemblyCopyright("No Copyright")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae3e3025-a56b-4901-88f8-86b18db9d987")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
