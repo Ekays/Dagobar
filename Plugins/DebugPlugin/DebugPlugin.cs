c676ad231f167ca83e05e71cc37e5aed3b9027de
BadCoder <alexandreviper@hotmail.fr>
2016-02-18 01:45:44 +0100

    Debug less annoying

--- Plugins/DebugPlugin/DebugPlugin.cs
+++ Plugins/DebugPlugin/DebugPlugin.cs
@@ -1,21 +1,25 @@
-﻿using Dagobar.Core.ChatProcessing;
+﻿#pragma warning disable 0162
+
+using Dagobar.Core.ChatProcessing;
 using System;
+
 
 namespace DebugPlugin
 {
     public class DebugPlugin : IPlugin
     {
+        private const bool DebugMode = false;
 
         public string Name { get { return "Debug"; } set { } }
 
         public void Initialize(IPluginContext context)
         {
-            context.Bot.SendChannelMessage("Debug plugin launched !");
+            if (DebugMode) context.Bot.SendChannelMessage("Debug plugin launched !");
         }
 
         public void PerformCommand(IPluginContext context)
         {
-            context.Bot.SendChannelMessage("Debug plugin processed !");
+            if (DebugMode) context.Bot.SendChannelMessage("Debug plugin processed !");
         }
     }
 }
