f16dfb5c4b0da8b796252f0e1313de1622ce06c7
BadCoder <alexandreviper@hotmail.fr>
2016-02-21 20:21:58 +0100

    Empty plugin

---
+++ WhoisPlugin/WhoisPlugin.cs
@@ -0,0 +1,20 @@
+﻿using Dagobar.Core.ChatProcessing;
+using System;
+
+namespace WhoisPlugin
+{
+    public class WhoisPlugin : IPlugin
+    {
+        public string Name { get { return "WhoIs"; } set { } }
+
+        public void Initialize(IPluginContext context)
+        {
+
+        }
+
+        public void PerformCommand(IPluginContext context)
+        {
+
+        }
+    }
+}
