ac2e0b0fb4e31067b8368a613509ca80bb14929b
BadCoder <alexandreviper@hotmail.fr>
2016-02-21 14:14:20 +0100

    Added copyrights headers on plugins sources files !

--- Plugins/MessagesPlugin/Command.cs
+++ Plugins/MessagesPlugin/Command.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ *          Dagobar is a Twitch bot with an UI which tends to be simple
+ *          Copyright (C) 2016 r00tKiller
+ *          This project is under license GNU GENERAL PUBLIC LICENSE Version 3
+ *          You can found a copy of this license at http://www.gnu.org/licenses/gpl-3.0.fr.html
+ *          
+ * */
 
 namespace MessagesPlugin
 {
--- Plugins/MessagesPlugin/MessagesPlugin.cs
+++ Plugins/MessagesPlugin/MessagesPlugin.cs
@@ -1,6 +1,13 @@
-﻿using Dagobar.Core.ChatProcessing;
+﻿/*
+ *          Dagobar is a Twitch bot with an UI which tends to be simple
+ *          Copyright (C) 2016 r00tKiller
+ *          This project is under license GNU GENERAL PUBLIC LICENSE Version 3
+ *          You can found a copy of this license at http://www.gnu.org/licenses/gpl-3.0.fr.html
+ *          
+ * */
+ 
+using Dagobar.Core.ChatProcessing;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
